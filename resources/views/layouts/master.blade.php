21d313d1d916b57029bb0d412d60977fdd830a1a
jakeb-k <jakeb.knowles@griffithuni.edu.au>
2023-10-26 15:30:21 +1000

    home page mobile opt

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -23,13 +23,14 @@
 
     </head>
     <body>
-        <div id="header">
+            <div id="header">
                     <div class="socials">
                         <div class="sRow">
                             <span class="icons"><i class="fa-brands fa-square-facebook" style="color: #ccff33;"></i></span>
                             <span class="icons"><i class="fa-brands fa-instagram" style="color: #ccff33;"></i></span>
                         </div>
 
+
                         <div class="sRow">
                             <a href='{{url("posts")}}'>
                                 <button class="tBtn">
@@ -73,75 +74,75 @@
                                     </svg>
                                 </button>
                             </a>
-                           
                         </div>
 
                     </div>
-
+                
                     <div class="title">
                         <h1>Freeman's<br><span style="color:white;">Mowing</span></h1>
                         <img src="{{url('images/mowgo.png')}}" />
                      </div>
-                </div>
+            </div>
     @yield('content')
-    <div id="footer">
+            <div id="footer">
                 <div class="socials">
                     <div class="sRow">
                         <span class="icons"><i class="fa-brands fa-square-facebook" style="color: #ccff33;"></i></span>
                         <span class="icons"><i class="fa-brands fa-instagram" style="color: #ccff33;"></i></span>
                     </div>
+
                     <div class="sRow">
-                            <a href='{{url("posts")}}'>
-                                <button class="tBtn">
-                                    BLOG
-                                    <svg width="79" height="46" viewBox="0 0 79 46" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <g filter="url(#filter0_f_618_1123)">
-                                        <path d="M42.9 2H76.5L34.5 44H2L42.9 2Z" fill="url(#paint0_linear_618_1123)"/>
-                                    </g>
-                                    <defs>
-                                        <filter id="filter0_f_618_1123" x="0" y="0" width="78.5" height="46" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                                        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-                                        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-                                        <feGaussianBlur stdDeviation="1" result="effect1_foregroundBlur_618_1123"/>
-                                        </filter>
-                                        <linearGradient id="paint0_linear_618_1123" x1="76.5" y1="2.00002" x2="34.5" y2="44" gradientUnits="userSpaceOnUse">
-                                        <stop stop-color="white" stop-opacity="0.6"/>
-                                        <stop offset="1" stop-color="white" stop-opacity="0.05"/>
-                                        </linearGradient>
-                                    </defs>
-                                    </svg>
-                                </button>
-                            </a>
-                            <a class="contactBtn">
-                                <button class="tBtn">
-                                    CONTACT
-                                    <svg width="79" height="46" viewBox="0 0 79 46" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <g filter="url(#filter0_f_618_1123)">
-                                        <path d="M42.9 2H76.5L34.5 44H2L42.9 2Z" fill="url(#paint0_linear_618_1123)"/>
-                                    </g>
-                                    <defs>
-                                        <filter id="filter0_f_618_1123" x="0" y="0" width="78.5" height="46" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                                        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-                                        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-                                        <feGaussianBlur stdDeviation="1" result="effect1_foregroundBlur_618_1123"/>
-                                        </filter>
-                                        <linearGradient id="paint0_linear_618_1123" x1="76.5" y1="2.00002" x2="34.5" y2="44" gradientUnits="userSpaceOnUse">
-                                        <stop stop-color="white" stop-opacity="0.6"/>
-                                        <stop offset="1" stop-color="white" stop-opacity="0.05"/>
-                                        </linearGradient>
-                                    </defs>
-                                    </svg>
-                                </button>
-                            </a>
-                           
-                        </div>
+                        <a href='{{url("posts")}}'>
+                            <button class="tBtn">
+                                BLOG
+                                <svg width="79" height="46" viewBox="0 0 79 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g filter="url(#filter0_f_618_1123)">
+                                    <path d="M42.9 2H76.5L34.5 44H2L42.9 2Z" fill="url(#paint0_linear_618_1123)"/>
+                                </g>
+                                <defs>
+                                    <filter id="filter0_f_618_1123" x="0" y="0" width="78.5" height="46" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                                    <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                                    <feGaussianBlur stdDeviation="1" result="effect1_foregroundBlur_618_1123"/>
+                                    </filter>
+                                    <linearGradient id="paint0_linear_618_1123" x1="76.5" y1="2.00002" x2="34.5" y2="44" gradientUnits="userSpaceOnUse">
+                                    <stop stop-color="white" stop-opacity="0.6"/>
+                                    <stop offset="1" stop-color="white" stop-opacity="0.05"/>
+                                    </linearGradient>
+                                </defs>
+                                </svg>
+                            </button>
+                        </a>
+                        <a class="contactBtn">
+                            <button class="tBtn">
+                                CONTACT
+                                <svg width="79" height="46" viewBox="0 0 79 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g filter="url(#filter0_f_618_1123)">
+                                    <path d="M42.9 2H76.5L34.5 44H2L42.9 2Z" fill="url(#paint0_linear_618_1123)"/>
+                                </g>
+                                <defs>
+                                    <filter id="filter0_f_618_1123" x="0" y="0" width="78.5" height="46" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                                    <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                                    <feGaussianBlur stdDeviation="1" result="effect1_foregroundBlur_618_1123"/>
+                                    </filter>
+                                    <linearGradient id="paint0_linear_618_1123" x1="76.5" y1="2.00002" x2="34.5" y2="44" gradientUnits="userSpaceOnUse">
+                                    <stop stop-color="white" stop-opacity="0.6"/>
+                                    <stop offset="1" stop-color="white" stop-opacity="0.05"/>
+                                    </linearGradient>
+                                </defs>
+                                </svg>
+                            </button>
+                        </a>       
+                    </div>
                 </div>
                 <div class="copyright">© 2023 JK Web Dev</div>
-                <div class="fTitle">
+                <div class="title">
                     <h1>Freeman's<br><span style="color:white;">Mowing</span></h1>
                 </div>
             </div>
-        </div>
+              
+  
     </body>
 </html>
 <script>
